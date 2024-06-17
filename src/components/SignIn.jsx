be254a845d214c3b363b8ba6dee7bb8e638f47c4
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-17 13:44:39 +0530

    updated

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
+    const [data,setData]=useState(
+        {
+        "email":"",
+        "password":""
+        }
+    )
+    const inputhandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
-        
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Email-Id</label>
+                            <input type="text" className="form-control" name='email' value={data.email} onChange={inputhandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="password" name="password" id="" className="form-control" value={data.password} onChange={inputhandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success" onClick={readValue}>Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
